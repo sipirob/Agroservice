c0d8ff167e9c372764a19a9833632be47ce3e13c
Robert Sipos <siposra@hotmail.com>
2019-02-15 13:49:08 +0100

    controller commentekkel ellátva

--- Agroservice/controller/AgroserviceController.cs
+++ Agroservice/controller/AgroserviceController.cs
@@ -23,46 +23,72 @@ namespace Agroservice.controller
             newWorkDataLoad = new model.NewWorkDataLoad();
             loadUsername = new model.LoadUsername();
         }
-       
+       /// <summary>
+       /// Ügyfelek adatainak lekérése listából
+       /// </summary>
+       /// <returns>Ügyfelek adatainak listája</returns>
         internal DataTable getClientData()
         {
             DataTable clientDT = model.ClientDataLoad.getClientDataFromList();
             return clientDT;
-          
         }
+        /// <summary>
+        /// Ügyfelek adatainak lekérése adatbázisból
+        /// </summary>
         public void loadClientData()
         {
             model.ClientDataLoad.clientDataLoad();
         }
+
+        /// <summary>
+        /// Dolgozók adatainak lekérése listából
+        /// </summary>
+        /// <returns>Dolgozók adatainak listája</returns>
         internal DataTable getWorkerData()
         {
             DataTable workerDT = model.WorkerDataLoad.getWorkerDataFromList();
             return workerDT;
         }
+        /// <summary>
+        /// Dolgozók adatainak lekérése adatbázisból
+        /// </summary>
         public void loadWorkerData()
         {
             model.WorkerDataLoad.workerDataLoad();
         }
 
       
-
+        /// <summary>
+        /// A még el nem végzett munkálatok listájának lekérése
+        /// </summary>
+        /// <returns>Az el nem végzett munkálatok listája</returns>
         internal DataTable getNewWorkData()
         {
             DataTable newWorkDT = model.NewWorkDataLoad.getClientDataFromList();
             return newWorkDT;
         }
+        /// <summary>
+        /// Az el nem végzett munkálatok lekérése adatbázisból
+        /// </summary>
         public void loadNewWorkData()
         {
             model.NewWorkDataLoad.newWorkDataLoad();
-            //model.NewWorkDataLoad.newWorkDataLoad();
+           
         }
 
+        /// <summary>
+        /// a belépett felhasználó nevének lekérése listából
+        /// </summary>
+        /// <returns>A belépett felhasználó neve</returns>
         internal string getloadUsername()
         {
             string username = model.LoadUsername.getUsername();
             return username;
                 
         }
+        /// <summary>
+        /// A belépett felhasználó nevének lekérdezése adatbázisból
+        /// </summary>
         public void usernameLoad()
         {
             model.LoadUsername.workerDataLoad();
